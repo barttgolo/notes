60c5b78c7f71d88e3ca5df7e6a663b62ce331fec
barttgolo <barttgolon@gmail.com>
2024-02-10 14:01:19 +0100

    feat: add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
-import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Metadata, Viewport } from "next";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+});
 
 export const viewport: Viewport = {
   themeColor: "#colorHere",
@@ -23,14 +28,15 @@ export const metadata: Metadata = {
     { rel: "icon", url: "icons/icon-128x128.png" },
   ],
 };
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body>
+    <html lang="en">
+      <body className={roboto.className}>
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
